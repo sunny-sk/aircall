099c53efc94ced38a522b8c29a0fcfa4e4875392
sunny-sk <sk956223@gmail.com>
2024-01-05 15:37:51 +0530

    header logo fixed

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 const Header = () => {
   return (
     <header className="bg-white h-18 border-gray-200 shadow-lg border-b flex items-center justify-between pl-5 pr-5 sm:pl-6 sm:pr-6 md:pr-14 fixed top-0 w-full z-30">
-      <img src="/src/assets/aircall-logo-vector.png" className="w-24 md:w-28" alt="logo" />
+      <img src="/aircall-logo-vector.png" className="w-24 md:w-28" alt="logo" />
       <div className="flex items-center justify-end space-x-3">
         <Avatar className="h-8 w-8 md:h-12 md:w-12">
           <AvatarImage src="https://media.licdn.com/dms/image/C5603AQHvK1BK9pzx5g/profile-displayphoto-shrink_200_200/0/1636993728551?e=1709769600&v=beta&t=dG8Hw_JB2J7d5HPQpxm10zp1HLREgXwPWFOujLirmNE" />
